f2f0dc14cde9813f2681f8484ce422c4eb5e4005
Christian <christian.bernhard97@web.de>
2022-03-29 22:54:03 +0200

    setup datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { DataTableComponent } from './data-table/data-table.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DataTableComponent,
   ],
   imports: [
     BrowserModule,
