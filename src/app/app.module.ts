2a74df91d01c24b73e5471f647b0415ca65282ba
Christian <christian.bernhard97@web.de>
2022-03-31 19:14:08 +0200

    refactored governmentService by using HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { DataTableComponent } from './data-table/data-table.component';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { DataChartComponent } from './data-chart/data-chart.component';
 
 import { NgxChartsModule }from '@swimlane/ngx-charts';
@@ -29,6 +31,7 @@ import { NgxChartsModule }from '@swimlane/ngx-charts';
     MatInputModule,
     MatPaginatorModule,
     NgxChartsModule,
+    HttpClient,
   ],
   providers: [],
   bootstrap: [AppComponent]
